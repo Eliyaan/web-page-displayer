aaf23b13e4e981a025a6167619ac2d09bb371150
Nopana <103932369+Eliyaan@users.noreply.github.com>
2024-06-16 15:48:25 +0200

    v bug to report

--- main.v
+++ main.v
@@ -1,4 +1,4 @@
-import net.http 
+import net.http
 
 enum Variant {
 	doctype
@@ -18,6 +18,7 @@ enum Variant {
 	nav
 	div
 	br
+	hr
 	script
 	a
 	html
@@ -26,93 +27,130 @@ enum Variant {
 	section
 	li
 	ul
+	span
+	g
+	h1
+	h2
+	h3
+	h4
+	h5
+	h6
+	p
+	code
+	strong
+	em
+	table
+	tr
+	td
+	pre
 }
 
 struct Balise { // replace with english name
 mut:
-	@type Variant
-	attr string // inside the balise
+	@type    Variant
+	attr     string // inside the balise
 	children []Balise
-	txt string // raw text between start/end of the balise
+	txt      string // raw text between start/end of the balise
 }
 
 fn main() {
 	println(get_tree())
 }
 
+fn escape_tag(main_content string, mut parents []Balise, mut stack []&Balise, nb_ int, in_balise_ bool) (int, bool) {
+	mut nb := nb_
+	in_balise := false
+	if stack.len == 1 {
+		dump(parents)
+		parents << *stack.pop()
+	} else {
+		stack.pop()
+	}
+	nb += 1
+	for main_content[nb] != `>` {
+		nb += 1
+	}
+	return nb, in_balise
+}
+
 fn get_tree() []Balise {
-	res := http.get("https://modules.vlang.io/os.html") or {panic("http get err: ${err}")}
+	res := http.get('https://modules.vlang.io/gg.html') or { panic('http get err: ${err}') }
 	mut main_content := res.body
 	mut parents := []Balise{}
 	mut stack := []&Balise{}
 	mut in_balise := false
 	mut nb := 0
-	for nb < main_content.len {
+	parse: for nb < main_content.len {
 		c := main_content[nb]
 		if in_balise {
 			if c == `/` {
-				dump("\\")
-				if stack.len == 1 {
-					parents << *stack.pop()
-				} else {
+				if main_content[nb + 1] == `>` {
+					nb, in_balise = escape_tag(main_content, mut parents, mut stack, nb,
+						in_balise)
+				}
+			} else if c == `>` {
+				in_balise = false
+				if stack.last().@type in [.br, .hr, .doctype, .meta, .link, .title, .path] {
 					stack.pop()
 				}
-				nb += 1
-				for main_content[nb] != `>` {
-					nb += 1
-				}
-				in_balise = false
-			} 
-			else if c == `>` {
-				dump(">")
-				in_balise = false
-				if stack.last().@type in [.br, .doctype, .meta, .link, .title, .path]{
-					stack.pop()
-				}
-			} 
-			else {
-				dump("attr ${c.ascii_str()}|")
-				//if !(c in [`\t`]) {
-					stack[stack.len-1].attr += c.ascii_str()
-				//}
+			} else {
+				stack[stack.len - 1].attr += c.ascii_str()
 			}
 		} else {
 			if c == `<` {
-				dump("<")
 				in_balise = true
-				mut name := ""
+				mut name := ''
 				nb += 1
-				for main_content[nb] != ` ` && main_content[nb] != `>` {
-					name += main_content[nb].ascii_str()	
-					nb += 1
-				}
-				if main_content[nb] == `>` {
-					in_balise = false
-				}
-				if name[0] == `!` {
-					if name == "!doctype" {
-						name = "doctype"
-					}
-				} else if name[0] == `c` {
-					if name == "clipPath" {
-						name = "clip_path"
-					}
-				}
-				if stack.len > 0 {
-					stack[stack.len-1].children << Balise{@type:Variant.from(name) or {panic("${err} : ${name}")}}
-					stack << &stack[stack.len-1].children[stack[stack.len-1].children.len-1]
+				if main_content[nb] == `/` {
+					nb, in_balise = escape_tag(main_content, mut parents, mut stack, nb,
+						in_balise)
 				} else {
-					stack << &Balise{@type:Variant.from(name) or {panic("${err} : ${name}")}}
+					for !(main_content[nb] in [` `, `>`, `\n`]) {
+						if main_content[nb] == `<` {
+							continue parse
+						}
+						name += main_content[nb].ascii_str()
+						nb += 1
+					}
+					name = name.to_lower()
+					if name[0] == `!` {
+						if name == '!doctype' {
+							name = 'doctype'
+						}
+					} else if name[0] == `c` {
+						if name == 'clipPath' {
+							name = 'clip_path'
+						}
+					}
+					if vari := Variant.from(name) {
+						if main_content[nb] == `>` {
+							in_balise = false
+						}
+						if stack.len > 0 {
+							stack[stack.len - 1].children << Balise{
+								@type: vari
+							}
+							stack << &stack[stack.len - 1].children[stack[stack.len - 1].children.len - 1]
+						} else {
+							stack << &Balise{
+								@type: vari
+							}
+						}
+					} else {
+						println('${err} : ${name}')
+						for main_content[nb] != `>` {
+							nb += 1
+						}
+					}
 				}
 			} else {
-				dump("txt ${c.ascii_str()}|")
-				if !(c in [`\n`, `\t`]) {
-					stack[stack.len-1].txt += c.ascii_str()
+				if c !in [`\n`, `\t`] {
+					stack[stack.len - 1].txt += c.ascii_str()
 				}
 			}
 		}
-		dump(stack)	
-		$dbg
+		dump(stack)
+		//		$dbg;
 		nb += 1
 	}
 	return parents
