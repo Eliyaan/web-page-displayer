2da0a7025f05592ae3dc21df89ff571f95a91822
Nopana <103932369+Eliyaan@users.noreply.github.com>
2024-06-30 14:05:28 +0200

    starting side bar

--- vlang_modules.v
+++ vlang_modules.v
@@ -14,10 +14,12 @@ mut:
 	max_w   int
 	line_h  int
 	content Balise
+	modules Balise
 }
 
 fn (mut r VlangModules) init() {
 	r.content = r.tree[0].get(.div, 'doc-content', '') or { panic('did not find elem in page') }
+	r.modules = r.tree[0].get(.nav, 'content hidden', '') or { panic('did not find elem in page') }
 	r.tree = []
 }
 
@@ -25,12 +27,48 @@ fn (mut r VlangModules) render(mut app App) {
 	if gg.window_size() != app.s_size {
 		app.s_size = gg.window_size()
 	}
-	app.ctx.draw_rect_filled(0, 0, app.s_size.width, app.s_size.height, gg.Color{26, 32, 44, 255})
+	app.ctx.draw_rect_filled(300, 0, app.s_size.width, app.s_size.height, gg.Color{26, 32, 44, 255})
+	app.ctx.draw_rect_filled(0, 0, 300, app.s_size.height, gg.Color{45, 55, 72, 255})
 	r.h = -app.scroll
-	r.show(app, mut r.content, 100, 1900, r.text_cfg, false)
+	r.show_modules(app, mut r.modules, 15, 300, r.text_cfg)
+	r.h = -app.scroll
+	r.show_content(app, mut r.content, 330, 1000, r.text_cfg, false)
 }
 
-fn (mut v VlangModules) show(app App, mut b Balise, offset int, width int, cfg gx.TextCfg, in_code bool) {
+fn (mut v VlangModules) show_modules(app App, mut b Balise, offset int, width int, cfg gx.TextCfg) {
+	text_cfg := gx.TextCfg{
+		size: cfg.size
+		color: gg.Color{255, 255, 255, 255}
+	}
+	v.line_h = (text_cfg.size * 6) / 5
+	if b.@type == .li {
+		v.h += v.line_h
+		if b.check_is(.li, 'open', '') {
+			v.w = 0
+		} else {
+			v.w = 20
+		}
+	}
+	if v.h < app.s_size.height {
+		for mut c in b.children {
+			match mut c {
+				Balise {
+					v.show_modules(app, mut c, offset, width, text_cfg)
+				}
+				RawText {
+					for t in c.txt.split('\n') {
+						if v.h >= 0 && t != '' {
+							app.ctx.draw_text(v.w + offset, v.h, c.txt, text_cfg)
+							v.w += (c.txt.len) * text_cfg.size / 2
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+fn (mut v VlangModules) show_content(app App, mut b Balise, offset int, width int, cfg gx.TextCfg, in_code bool) {
 	mut code := in_code
 	size := match true {
 		b.check_is(.h1, '', '') { 26 }
@@ -77,23 +115,29 @@ fn (mut v VlangModules) show(app App, mut b Balise, offset int, width int, cfg g
 		for mut c in b.children {
 			match mut c {
 				Balise {
-					v.show(app, mut c, offset, width, text_cfg, code)
+					v.show_content(app, mut c, offset, width, text_cfg, code)
 				}
 				RawText {
 					if c.txt != linebreaks#[..c.txt.len] || in_code || code {
 						s := c.txt.split('\n')
 						for n, t in s {
 							if v.h >= 0 {
-								if v.w >= 0 && v.w < width {
+								if v.w >= 0 && v.w + t.len * text_cfg.size / 2 < width {
 									app.ctx.draw_text(v.w + offset, v.h, t, text_cfg)
 									v.w += (t.len) * text_cfg.size / 2
 									if v.w > v.max_w {
 										v.max_w = v.w
 									}
 								} else {
+									mut i := (width - v.w) / (text_cfg.size / 2)
+									for i != -1 && t[i] != ` ` {
+										i -= 1
+									}
+									i += 1
+									app.ctx.draw_text(v.w + offset, v.h, t[..i], text_cfg)
 									v.h += v.line_h
-									v.w = 0
-									// handle line break
+									app.ctx.draw_text(offset, v.h, t[i..], text_cfg)
+									v.w = t[i..].len * text_cfg.size / 2
 								}
 							}
 							if n < s.len - 1 && s.len > 1 {
